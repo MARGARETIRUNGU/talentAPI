5dd32dc56bca821c5bb65d678135d401f6398b9a
CharlesMuvaka <muvakacharles.com>
2024-02-15 20:53:45 +0300

    extend the jpa repository to manipulate the database

---
+++ src/main/java/com/example/talent_man/repos/AssessmentQuestionsRepo.java
@@ -0,0 +1,7 @@
+package com.example.talent_man.repos;
+
+import com.example.talent_man.models.AssessmentQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssessmentQuestionsRepo extends JpaRepository<AssessmentQuestion, Integer> {
+}

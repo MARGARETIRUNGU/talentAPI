f248298771bd9e6f27d1e8a8729eed3e397ee0f2
CharlesMuvaka <muvakacharles.com>
2024-02-15 09:51:55 +0300

    initial commit

---
+++ src/main/java/com/example/talent_man/TalentManApplication.java
@@ -0,0 +1,13 @@
+package com.example.talent_man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentManApplication.class, args);
+	}
+
+}

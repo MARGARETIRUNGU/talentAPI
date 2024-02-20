d27be6c5c779e79b637af422b29682b567888fd1
CharlesMuvaka <muvakacharles.com>
2024-02-20 05:35:03 +0300

    feat: use the repository instance to add a manager into the db.

--- src/main/java/com/example/talent_man/service_imp/UserServiceImp.java
+++ src/main/java/com/example/talent_man/service_imp/UserServiceImp.java
@@ -1,7 +1,8 @@
 package com.example.talent_man.service_imp;
 
+import com.example.talent_man.models.user.Manager;
 import com.example.talent_man.models.user.User;
-import com.example.talent_man.repos.UserRepo;
+import com.example.talent_man.repos.user.UserRepo;
 import com.example.talent_man.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,12 @@ public class UserServiceImp implements UserService {
     @Autowired
     private UserRepo repo;
     @Override
-    public User addUser(User user) {
+    public Manager addUser(Manager user) {
         return repo.save(user);
+    }
+
+    @Override
+    public Manager getManagerById(int id) {
+        return repo.getReferenceById(id);
     }
 }

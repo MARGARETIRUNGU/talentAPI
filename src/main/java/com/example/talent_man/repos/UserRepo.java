51bbd919f2173b41992e0dff049c045aa378ed63
CharlesMuvaka <muvakacharles.com>
2024-02-20 05:39:39 +0300

    fix: delete unwanted models.

--- src/main/java/com/example/talent_man/repos/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.talent_man.repos;
-
-import com.example.talent_man.models.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-}

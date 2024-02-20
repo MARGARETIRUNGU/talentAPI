42729c91098ace3fda4ce6f21ee033fd3d3e1d13
CharlesMuvaka <muvakacharles.com>
2024-02-20 05:20:39 +0300

    feat: define an instance to initialise a choice instance.

---
+++ src/main/java/com/example/talent_man/models/Choice.java
@@ -0,0 +1,28 @@
+package com.example.talent_man.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "choices")
+@NoArgsConstructor
+public class Choice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "choice_id", columnDefinition = "INTEGER")
+    private int choice_id;
+
+    @Column(name = "choice_description", columnDefinition = "VARCHAR(255)")
+    private String choiceDescription;
+    @Column(name = "choice_value", columnDefinition = "INTEGER")
+    private int choiceValue;
+
+    @OneToMany(mappedBy = "choice")
+    Set<UserQuestionAnswer> userAnswers;
+
+
+}

fa4a7bc60c29d809a197e5a0e9edb2da7964bae9
CharlesMuvaka <muvakacharles.com>
2024-02-20 05:21:43 +0300

    feat: add a no args constructor, getters and setters.

--- src/main/java/com/example/talent_man/models/user/Manager.java
+++ src/main/java/com/example/talent_man/models/user/Manager.java
@@ -2,7 +2,6 @@ package com.example.talent_man.models.user;
 
 import com.example.talent_man.models.PotentialAttribute;
 import jakarta.persistence.*;
-import lombok.Data;
 
 import java.util.Set;
 
@@ -10,12 +9,29 @@ import java.util.Set;
 public class Manager extends User{
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "manager_id")
-    public Set<PotentialAttribute> potentialAttributeSet;
+    private Set<PotentialAttribute> potentialAttributeSet;
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "manager_id")
-    public Set<Employee> employees;
+    private Set<Employee> employees;
 
+    public Manager(){}
     public Manager(String name,int roleId){
         super(name, roleId);
+    }
+
+    public Set<PotentialAttribute> getPotentialAttributeSet() {
+        return potentialAttributeSet;
+    }
+
+    public void setPotentialAttributeSet(Set<PotentialAttribute> potentialAttributeSet) {
+        this.potentialAttributeSet = potentialAttributeSet;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
     }
 }
